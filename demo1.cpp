d080cf99d3d45fc41b00fe15a2201d5410045181
zhanglei <676269689@qq.com>
2015-09-17 01:36:29 +0800

    第一天 学习程序设计基础 笔记和代码

---
+++ demo1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+//    cout << 7.9*0.8+9.5*1.5+8.7*0.6 << endl;
+//    cout<< 3+4 <<endl;
+//    cout << 3/2 << endl;//除实际是整除
+//    cout << sin(3.1415926/9) << endl;
+//    cout << tan(-3.14*4/3) << endl;
+//    cout << sqrt(10*10-2*3*4) << endl;
+    cout << 3 - 4;
+    return 0;
+}
